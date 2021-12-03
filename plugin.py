70062d30f7b3a7e4263b3d9376b6f5cc3cd96d80
KwonIkYoung <worms112@naver.com>
2021-12-03 09:01:20 +0900

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -42,12 +42,12 @@ menu = {
 }
 
 plugin_info = {
-    'version' : '0.1.0.0',
-    'name' : 'webtoon_daum',
+    'version' : '0.1.1.0',
+    'name' : 'webtoon_daum_2',
     'category_name' : 'service',
     'developer' : 'soju6jan',
-    'description' : u'다음 웹툰 다운로드',
-    'home' : 'https://github.com/soju6jan/webtoon_daum',
+    'description' : u'다음 웹툰 다운로드 2',
+    'home' : 'https://github.com/KwonIkYoung/webtoon_daum',
     'more' : '',
 }
 
